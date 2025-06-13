c5283e68b1af92316a97686c3096a29962169d5b
KleberRhuan <kleber_rhuan@hotmail.com>
2025-06-13 20:15:12 -0300

    refactor: atualiza aplicação principal com novas configurações
    - Adiciona anotações para configurações de infraestrutura
    - Configura scan de componentes
    - Atualiza configurações de inicialização

--- src/main/java/com/kleberrhuan/houer/HouerApplication.java
+++ src/main/java/com/kleberrhuan/houer/HouerApplication.java
@@ -1,12 +1,17 @@
+/* (C)2025 Kleber Rhuan */
 package com.kleberrhuan.houer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@ConfigurationPropertiesScan("com.kleberrhuan.houer.common.infra.properties")
 @SpringBootApplication
 public class HouerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HouerApplication.class, args);
-    }
 
+  public static void main(String[] args) {
+    SpringApplication.run(HouerApplication.class, args);
+  }
 }
