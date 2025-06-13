df923240b8a438c4ac3a9b213aeba7e76bf62a0d
KleberRhuan <kleber_rhuan@hotmail.com>
2025-06-13 11:33:53 -0300

    init: Estrutura Base do Projeto

---
+++ src/main/java/com/kleberrhuan/houer/HouerApplication.java
@@ -0,0 +1,12 @@
+package com.kleberrhuan.houer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HouerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kleberrhuan/houer/HouerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleberrhuan.houer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
