8f7b966fd49c458ebe8b6d40570b3300cb1809b2
KleberRhuan <kleber_rhuan@hotmail.com>
2025-06-13 20:17:18 -0300

    feat: adiciona método parse a ConstraintParser
    - Define método para analisar SQLException e retornar informações de violação
    - Adiciona anotações e importações necessárias

--- src/main/java/com/kleberrhuan/houer/common/application/port/persistence/ConstraintParser.java
+++ src/main/java/com/kleberrhuan/houer/common/application/port/persistence/ConstraintParser.java
@@ -1,4 +1,11 @@
+/* (C)2025 Kleber Rhuan */
 package com.kleberrhuan.houer.common.application.port.persistence;
 
+import com.kleberrhuan.houer.common.interfaces.dto.error.ViolationInfo;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@FunctionalInterface
 public interface ConstraintParser {
+  Optional<ViolationInfo> parse(SQLException ex);
 }
