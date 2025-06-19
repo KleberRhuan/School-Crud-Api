126a654f216c0a19253d883338eea5d662a6b806
Kleber Rhuan <113813046+KleberRhuan@users.noreply.github.com>
2025-06-19 20:46:02 -0300

    Fix: Comente para ref incorreta na migration 18

--- src/main/resources/db/migration/V18__add_version_to_auditable_tables.sql
+++ src/main/resources/db/migration/V18__add_version_to_auditable_tables.sql
@@ -5,7 +5,6 @@ ADD COLUMN IF NOT EXISTS updated_by BIGINT REFERENCES account.users(id),
 ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP WITH TIME ZONE;
 
 -- Comentários
-COMMENT ON COLUMN school.school.version IS 'Versão para controle de concorrência otimista';
 COMMENT ON COLUMN school.school_metrics_jsonb.created_at IS 'Data de criação (campo Auditable)';
 COMMENT ON COLUMN school.school_metrics_jsonb.updated_at IS 'Data da última modificação (campo Auditable)';
 
