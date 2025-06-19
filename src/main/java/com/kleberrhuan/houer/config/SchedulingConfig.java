7e4f20f22e8c274a0ebe7a57ea4e553964505cf0
KleberRhuan <kleber_rhuan@hotmail.com>
2025-06-18 21:38:38 -0300

    config: adiciona configurações específicas para módulos auth e school

---
+++ src/main/java/com/kleberrhuan/houer/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+/* (C)2025 Kleber Rhuan */
+package com.kleberrhuan.houer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+  // Configuração para habilitar o agendamento de tarefas
+}
