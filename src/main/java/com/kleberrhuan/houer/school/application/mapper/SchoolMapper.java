783e7de48c2ee955997f017f1ebd39d4e8811cd2
KleberRhuan <kleber_rhuan@hotmail.com>
2025-06-20 10:17:22 -0300

    chore: remove `SchoolServiceTest` and standardize code formatting across services
    Deletes unused SchoolServiceTest class, adjusts indentation, and reorganizes imports for consistency in formatting throughout services and controller layers.

--- src/main/java/com/kleberrhuan/houer/school/application/mapper/SchoolMapper.java
+++ src/main/java/com/kleberrhuan/houer/school/application/mapper/SchoolMapper.java
@@ -47,34 +47,34 @@ public interface SchoolMapper {
   @Mapping(target = "updatedAt", ignore = true)
   SchoolMetricsDto metricsToDto(SchoolMetrics metrics);
 
-    @Mapping(target = "code", ignore = true)
-    @Mapping(target = "schoolMetrics", ignore = true)
-    @Mapping(target = "nomeEsc", source = "schoolName")
-    @Mapping(target = "nomeDep", source = "administrativeDependency")
-    @Mapping(target = "de", source = "stateCode")
-    @Mapping(target = "mun", source = "municipality")
-    @Mapping(target = "distr", source = "district")
-    @Mapping(target = "tipoEsc", source = "schoolType")
-    @Mapping(target = "tipoEscDesc", source = "schoolTypeDescription")
-    @Mapping(target = "codsit", source = "situationCode")
-    @Mapping(target = "codesc", source = "schoolCode")
-    @BeanMapping(
-            nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
-    )
-    void updateEntity(@MappingTarget School school, SchoolUpdateRequest request);
+  @Mapping(target = "code", ignore = true)
+  @Mapping(target = "schoolMetrics", ignore = true)
+  @Mapping(target = "nomeEsc", source = "schoolName")
+  @Mapping(target = "nomeDep", source = "administrativeDependency")
+  @Mapping(target = "de", source = "stateCode")
+  @Mapping(target = "mun", source = "municipality")
+  @Mapping(target = "distr", source = "district")
+  @Mapping(target = "tipoEsc", source = "schoolType")
+  @Mapping(target = "tipoEscDesc", source = "schoolTypeDescription")
+  @Mapping(target = "codsit", source = "situationCode")
+  @Mapping(target = "codesc", source = "schoolCode")
+  @BeanMapping(
+          nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+  )
+  void updateEntity(@MappingTarget School school, SchoolUpdateRequest request);
 
-    @Mapping(target = "nomeEsc", source = "schoolName")
-    @Mapping(target = "nomeDep", source = "administrativeDependency")
-    @Mapping(target = "de", source = "stateCode")
-    @Mapping(target = "mun", source = "municipality")
-    @Mapping(target = "distr", source = "district")
-    @Mapping(target = "tipoEsc", source = "schoolType")
-    @Mapping(target = "tipoEscDesc", source = "schoolTypeDescription")
-    @Mapping(target = "codsit", source = "situationCode")
-    @Mapping(target = "codesc", source = "schoolCode")
-    @Mapping(target = "schoolMetrics", ignore = true)
-    @Mapping(target = "code", ignore = true)
-    School toEntity(SchoolCreateRequest request);
+  @Mapping(target = "nomeEsc", source = "schoolName")
+  @Mapping(target = "nomeDep", source = "administrativeDependency")
+  @Mapping(target = "de", source = "stateCode")
+  @Mapping(target = "mun", source = "municipality")
+  @Mapping(target = "distr", source = "district")
+  @Mapping(target = "tipoEsc", source = "schoolType")
+  @Mapping(target = "tipoEscDesc", source = "schoolTypeDescription")
+  @Mapping(target = "codsit", source = "situationCode")
+  @Mapping(target = "codesc", source = "schoolCode")
+  @Mapping(target = "schoolMetrics", ignore = true)
+  @Mapping(target = "code", ignore = true)
+  School toEntity(SchoolCreateRequest request);
 
   @Named("localDateTimeToInstant")
   default Instant localDateTimeToInstant(LocalDateTime localDateTime) {
